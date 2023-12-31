1e76b9db55609809e9823568a508ec319a9d47ea
dmyn1993 <dmyn1993@gmail.com>
2023-12-31 23:42:50 +0800

    多个启动器图标

---
+++ app/src/main/java/com/testapp/FirstActivity.java
@@ -0,0 +1,18 @@
+package com.testapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+public class FirstActivity extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+		Intent broadcastIntent = new Intent("com.testapp.First");
+        sendBroadcast(broadcastIntent);
+		finish();
+    }
+    
+}
